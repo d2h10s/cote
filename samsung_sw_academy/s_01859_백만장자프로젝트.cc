6ce25e6b354507aeccbdea8dc7a13b12e1f66e4c
d2h10s <ecanus9@gmail.com>
2021-03-28 16:16:09 +0900

    <docs> 백만장자프로젝트 코드 수정

--- samsung_sw_academy/s_01859_백만장자프로젝트.cc
+++ samsung_sw_academy/s_01859_백만장자프로젝트.cc
@@ -1,29 +1,31 @@
 #include<iostream>
 using namespace std;
-
-int prices[1000000];
-
-int max(int s, int e){
-    int idx = s;
-    for(int i = s; i < e; i++)
+ 
+int prices[1000000], max_price[1000000];
+ 
+int max(int N){
+    int idx = N - 1;
+    for(int i = N - 1; i >= 0; i--){
         if (*(prices + idx) < *(prices + i))
             idx = i;
-    return idx;
+        max_price[i] = idx;
+    }
 }
-
+ 
 int main(int argc, char** argv){
-	int i, test_case, T, N;
-    freopen("input.txt", "r", stdin);
-	cin>> T;
-	for (test_case = 1; test_case <= T; ++test_case) {
-        cin >> N;
-    	long long int num_stock = 0, balance = 0;
-        for (i = 0; i < N; i++) cin >> *(prices+i);
-        int idx = max(0, N);
-        
+    int i, test_case, T, N;
+    //freopen("input.txt", "r", stdin);
+    cin>> T;
+    for (test_case = 1; test_case <= T; ++test_case) {
+        scanf("%d", &N);
+        long long int num_stock = 0, balance = 0;
+        for (i = 0; i < N; i++) scanf("%d", prices+i);
+        max(N);
+        int idx = max_price[0];
+         
         for (i = 0; i < N; i++) {
             if (idx <= i) {
-                idx = max(i + 1, N);
+                idx = max_price[i + 1];
                 balance +=  *(prices + i) * num_stock;
                 num_stock = 0;
             }
@@ -34,6 +36,6 @@ int main(int argc, char** argv){
         }
         if(num_stock) balance += prices[i] * num_stock;
         printf("#%d %lld\n", test_case, balance);
-	}
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+    }
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
