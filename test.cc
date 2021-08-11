9702f82d75eabbc2f46b7987bdf6a17ad1452f76
d2h10s <ecanus9@gmail.com>
2021-08-11 21:05:42 +0900

    test file update

--- test.cc
+++ test.cc
@@ -1,24 +1,10 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
 
-string& g(string& s){
-    for(auto it = s.begin(); it != s.end(); it++){
-        if (*it == '1') *it = '0';
-        else *it = '1';
-    }
-    return s;
+void print(int a, int b= 3){
+    std::cout << a << ' ' << b << std::endl;
 }
 
-string f(const string& s){
-    string new_s;
-    for(auto it = s.end() - 1; it >= s.begin(); it--){
-        new_s += *it;
-    }
-    return new_s;
-}
-
-int main(int argc, char** arg){
-    string s = "0";
-    cout << f(s);
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+int main(){
+    print(1,3);
+    print(1);
 }
--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-def c(n,r):
-    isVisited = [True]+[False for _ in range(n)]
-    i = 1
-    pre_i = 1
-    temp = []
-    result = []
-    while True:
-        if len(temp) == r:
-            result.append(temp)
-            print(temp)
-            temp.pop()
-        if all(isVisited) == True:
-            break
-        if isVisited[i] == False:
-            isVisited[i] = True
-            temp.append(i)
-        else:
-            isVisited[pre_i] = False
-            temp.pop()
+a = 3
 
-c(3,2)
+if 10 <= a and a <= 20:
+    print("10 초과")
